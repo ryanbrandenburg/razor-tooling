2ed4b946a442a2b0f411991df7c279a29b1cd3e2
Allison Chou <allichou@microsoft.com>
2021-12-06 22:10:47 -0800

    Add RazorOpenDocumentService

---
+++ src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/RazorOpenDocumentService.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+#nullable enable
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.ExternalAccess.Razor;
+using Microsoft.CodeAnalysis.Razor.ProjectSystem;
+
+namespace Microsoft.CodeAnalysis.Razor.Workspaces
+{
+    internal class RazorOpenDocumentService : IRazorDocumentOpenService
+    {
+        private const string RazorGeneratedDocumentExtension = ".g.cs";
+
+        private readonly ProjectSnapshotManagerDispatcher _projectSnapshotManagerDispatcher;
+        private readonly ProjectSnapshotManager _projectSnapshotManager;
+
+        public RazorOpenDocumentService(ProjectSnapshotManagerDispatcher projectSnapshotManagerDispatcher, ProjectSnapshotManager projectSnapshotManager)
+        {
+            if (projectSnapshotManagerDispatcher is null)
+            {
+                throw new ArgumentNullException(nameof(projectSnapshotManagerDispatcher));
+            }
+
+            if (projectSnapshotManager is null)
+            {
+                throw new ArgumentNullException(nameof(projectSnapshotManager));
+            }
+
+            _projectSnapshotManagerDispatcher = projectSnapshotManagerDispatcher;
+            _projectSnapshotManager = projectSnapshotManager;
+        }
+
+        public async Task<bool> IsDocumentOpenAsync(Document generatedDocument, CancellationToken cancellationToken)
+        {
+            if (!TryGetRazorDocumentPath(generatedDocument, out var razorDocumentPath))
+            {
+                return false;
+            }
+
+            var isOpen = await _projectSnapshotManagerDispatcher.RunOnDispatcherThreadAsync(
+                () => _projectSnapshotManager.IsDocumentOpen(razorDocumentPath), cancellationToken).ConfigureAwait(false);
+            return isOpen;
+        }
+
+        private static bool TryGetRazorDocumentPath(Document generatedDocument, out string? razorDocumentPath)
+        {
+            if (generatedDocument.FilePath is null || !generatedDocument.FilePath.EndsWith(RazorGeneratedDocumentExtension))
+            {
+                razorDocumentPath = null;
+                return false;
+            }
+
+            razorDocumentPath = generatedDocument.FilePath.Substring(
+                0, generatedDocument.FilePath.Length - RazorGeneratedDocumentExtension.Length);
+            return true;
+
+        }
+    }
+}
